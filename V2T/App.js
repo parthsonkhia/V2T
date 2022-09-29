a92ac2fbc44d6f00f378cc65d9f5a2624ab19617
Mihir Bhansali <mbmihirbhansali@gmail.com>
2022-09-28 20:35:42 -0400

    roster page updated

--- V2T/App.js
+++ V2T/App.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './screens/login/login';
 import Register from './screens/register/register';
+import Roster from './screens/roster/roster';
 
 export default function App() {
   return (
     <View>
       {/* <Login /> */}
-      <Register />
+      {/* <Register /> */}
+      <Roster />
     </View>
   );
 }
